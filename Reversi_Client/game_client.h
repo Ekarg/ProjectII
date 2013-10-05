2e4a6a4a972daf2bfb689cc2c460e27b99f795b3
Ekarg <coffmangrace@yahoo.com>
2013-10-04 23:51:26 -0600

    Setting up client side
    This code does not run.

--- Reversi_Client/game_client.h
+++ Reversi_Client/game_client.h
@@ -1,15 +1,36 @@
+/* 
+    File: game_client.h
 
+    Authors: Grace Coffman, Sidian Wu, Alejandro Vega, Wesley Tang, Violeta Torres
+            Department of Computer Science
+            Texas A&M University
+			
+    This file contains the fuctions for the Game_Client which will communicate with the server 
+	that is housing the application to play a game of Reversi
+
+*/
 #ifndef _GAME_CLIENT_
 #define _GAME_CLIENT_
 
+#include <string>
 
 class Game_Client {
 private:
 
 
 public:
-
-
+	
+	bool send_cmd(string s);
+	// Takes in the user command and sends it to the server	
+	// The response from the server is printed out in this function to the main screen
+	// The return value specifies if the game has ended and the connection should be termintated
+	
+	bool connect(int port);
+	// Takes the port number entered by the user and attempts to connect. 	
+	
+	bool disconnect();
+	// Close socket connection
+	
 };
 
 #endif
---
+++ Reversi_Client/main.cpp
@@ -0,0 +1,23 @@
+/* 
+    File: main.cpp
+
+    Authors: Grace Coffman, Sidian Wu, Alejandro Vega, Wesley Tang, Violeta Torres
+            Department of Computer Science
+            Texas A&M University
+			
+    This file contains the main function that will handle the start up of the Game_Client
+
+*/
+#include "game_client.h"
+
+int main(int argc, char* argv[])
+{
+	//Initialize game_server
+	
+	//begin accepting user inputs and use game client to send
+	
+	//if exit command is processed by game_client, break from while loop
+	
+	return 0;
+
+}
